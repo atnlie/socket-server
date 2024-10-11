61924ae54d57f65cec833fa9f684b864121f2075
Anton <heni.kurniyanto@kuncie.com>
2024-10-11 10:15:58 +0700

    feat(log): add log details

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,14 @@ import { createServer } from 'http';
 const WebSocket = require("ws");
 
 //client websocket
-// const client = new WebSocket("ws://192.168.101.47:4004/event_channel/0/a5883093ecaf36c5/NFV4-FR");
+const clientFR = new WebSocket("ws://192.168.101.47:4004/event_channel/0/a5883093ecaf36c5/NFV4-FR");
+clientFR.on("open", () => {
+    console.log("Client connected");
+});
+
+clientFR.on("close", () => {
+    console.log("Client disconnected");
+})
 
 // density
 // counting
@@ -28,6 +35,7 @@ const server = createServer()
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
+        // origin: "https://00ad-116-206-242-110.ngrok-free.app",
         credentials: true,
         methods: ["GET", "POST"],
     }
@@ -37,7 +45,12 @@ io.on('connection', (socket) => {
     console.log('Socket connection connected');
 
     client.on("message", (msg: any) => {
-        console.log('DATA =>\n' + msg + "\n\n");
+        console.log('PEOPLE =>\n' + msg + "\n\n");
+        io.emit("message", JSON.parse(msg));
+    })
+
+    clientFR.on("message", (msg: any) => {
+        console.log('FR =>\n' + msg + "\n\n");
         io.emit("message", JSON.parse(msg));
     })
 });
